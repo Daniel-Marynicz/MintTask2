01919417f67a946a797c660a6c53dbbcdab63767
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-04-28 23:42:10 +0200

    down migration fix

--- backend/src/Migrations/Version20200427172921.php
+++ backend/src/Migrations/Version20200427172921.php
@@ -39,7 +39,6 @@ final class Version20200427172921 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
         $this->addSql('DROP TABLE "user"');
     }
