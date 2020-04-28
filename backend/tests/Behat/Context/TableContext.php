98c8ff8b325b2947edcbb23febba568046531998
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-04-28 14:38:15 +0200

    count rows in TableContext::theDataOfTheTableShouldMatch

--- backend/tests/Behat/Context/TableContext.php
+++ backend/tests/Behat/Context/TableContext.php
@@ -9,6 +9,7 @@ use Behat\MinkExtension\Context\RawMinkContext;
 use Behatch\Asserter;
 use Behatch\Html;
 use function array_values;
+use function count;
 use function implode;
 use function sprintf;
 
@@ -26,8 +27,11 @@ class TableContext extends RawMinkContext
      */
     public function theDataOfTheTableShouldMatch(string $table, TableNode $tableNode) : void
     {
-        $rowsSelector = sprintf('%s tbody tr', $table);
-        $htmlRows     = $this->getSession()->getPage()->findAll('css', $rowsSelector);
+        $rowsSelector      = sprintf('%s tbody tr', $table);
+        $htmlRows          = $this->getSession()->getPage()->findAll('css', $rowsSelector);
+        $actualCountRows   = count($htmlRows);
+        $expectedCountRows = count($tableNode->getRows()) - 1;
+        $this->assertEquals($expectedCountRows, $actualCountRows);
 
         foreach ($tableNode as $index => $row) {
             $expectedText = implode(' ', array_values($row));
